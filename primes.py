519f2b8e2340a614351cc6966cfb43ebdb519a7a
Shabrina Hoque <hoqueshabrina@gmail.com>
2022-10-17 16:27:37 +0100

    ValueError

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    listprime = []
+    if number_of_primes <= 0:
+        raise ValueError
+    for x in range(2,10000000):
+        for y in range(2,x):
+            if x%y==0:
+                break
+        else:  
+            listprime.append(x)
+
+            if len(listprime) == int(number_of_primes):
+                break
+
+    return listprime
